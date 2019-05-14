d16f1befbbbb75b0c2cf76b16aafc882f6551373
vladimir kanarik <kaner2213@gmail.com>
2019-05-15 00:13:04 +0300

    init

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import serial
 ser = serial.Serial('/dev/ttyUSB0', 9600)
-ser.write(b'3')
-ser.write(b'5')
-ser.write(b'7')
+ser.flushInput()
+
+while True:
+    if ser.inWaitin()>0:
+        inputValue = ser.read(1)
+        print(inputValue)
